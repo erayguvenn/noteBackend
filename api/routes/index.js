86fadb333b41f3d2ea0bcf5cd4c5cd4d22cae607
Ömer Faruk BARAN <of123baran@outlook.com>
2023-01-03 15:34:05 +0300

    Update index.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import AuthRouter from "./auth/index.js"
 import NoteRouter from "./note/index.js"
+import UserRouter from "./user/index.js"
 
 const router = express.Router();
 
 router.use("/note", NoteRouter);
 router.use("/auth", AuthRouter);
+router.use("/user", UserRouter);
 
 router.get("/", (req, res) => {
     res.send("Api rotasını buldun")
