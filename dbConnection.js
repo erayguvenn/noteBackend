890bc005fa56e6fd1608bd0ff3aefba6e6ced1c7
Ömer Faruk BARAN <of123baran@outlook.com>
2023-01-22 12:12:01 +0300

    Update dbConnection.js

--- dbConnection.js
+++ dbConnection.js
@@ -4,12 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 const connection = await mysql.createConnection(process.env.DB_CONNECTION);
 
+
+setInterval(keepAlive, 1000 * 60 * 5);
+
 async function keepAlive(){
     await connection.query("SELECT 1");
     console.log("DB bağlantısı sağlandı");
 }
-while (true) {
-    keepAlive();
-    new Promise(resolve => setTimeout(resolve, 1000 * 60 * 5));
-}
+
 export default connection;
