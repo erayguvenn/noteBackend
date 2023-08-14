bd8a33a77c2b8d0fe7295b3924b244be86af773f
Ömer Faruk BARAN <of123baran@outlook.com>
2023-08-14 22:48:14 +0300

    HTTPS support

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ import express, { json, urlencoded } from "express";
 import cors from "cors";
 import ApiRouter from "./api/routes/index.js"
 import dotenv from "dotenv";
+import fs from "fs";
+import * as https from "https";
 
 
 dotenv.config();
-
 const app = express();
+var privateKey = fs.readFileSync( 'certificates/notebackend.pem' );
+var certificate = fs.readFileSync( 'certificates/notebackend.crt' );
+
+https.createServer({
+    key: privateKey,
+    cert: certificate
+}, app);
 
 const PORT = process.env.PORT || 3000;
 
